dcf311ac39f5e666bd52ff88882969c2240e4d5d
Benelkadi-Ahmed-Malik <ahmed-malik.ben-elkadi@laplateforme.io>
2023-03-10 10:24:49 +0100

    [ADD] Last push

--- Class/Combat.py
+++ Class/Combat.py
@@ -4,36 +4,46 @@ import random
 import json
 
 class Combat:
+    """Classe représentant un combat entre deux Pokémon
+    Attributs :
+        - pokemon1 : le premier Pokémon
+        - pokemon2 : le second Pokémon
+        - pokedex : le Pokédex
+        """
     def __init__(self, pokemon1, pokemon2, pokedex):
         self.__pokemon1 = pokemon1
         self.__pokemon2 = pokemon2
         self.__pokedex = pokedex
         self.__tour = 1
 
-    def est_finie(self):
-        if self.__pokemon1.get_pv() <= 0 or self.__pokemon2.get_pv() <= 0:
-            return True
+    def fini(self):
+        if self.__pokemon1.get_pv() <= 0 or self.__pokemon2.get_pv() <= 0:  # Si un des deux Pokémon est K.O. le combat est terminé
+            return True # Le combat est terminé
         else:
-            return False
+            return False # Le combat n'est pas terminé
 
-    def get_vainqueur(self):
-        if self.__pokemon1.get_pv() <= 0:
-            return self.__pokemon2.get_nom()
-        elif self.__pokemon2.get_pv() <= 0:
-            return self.__pokemon1.get_nom()
+    def get_vainqueur(self): # Renvoie le nom du vainqueur
+        if self.__pokemon1.get_pv() <= 0: # Si le Pokémon 1 est K.O. le Pokémon 2 est le vainqueur
+            return self.__pokemon2.get_nom()  # Renvoie le nom du Pokémon 2
+        elif self.__pokemon2.get_pv() <= 0: # Si le Pokémon 2 est K.O. le Pokémon 1 est le vainqueur
+            return self.__pokemon1.get_nom() # Renvoie le nom du Pokémon 1
         else:
             return None
 
-    def attaque_possible(self):
+    def attaque_possible(self): # Renvoie True si le Pokémon 1 attaque, False si le Pokémon 2 attaque
         return random.randint(0, 1)
 
-    def calculer_degats(self, attaquant, defenseur):
+    def calculer_degats(self, attaquant, defenseur): # Calcul les dégâts infligés par le Pokémon attaquant au Pokémon défenseur
         TABLE_TYPES = []
 
-        for types, multiplicateur in TABLE_TYPES:
+        i = 0
+        found = False
+        while i < len(TABLE_TYPES) and not found:
+            types, multiplicateur = TABLE_TYPES[i]
             if attaquant.type_pokemon in types and defenseur.type_pokemon in types:
                 multiplicateur_type = multiplicateur
-                break
+                found = True
+            i += 1
         else:
             multiplicateur_type = 1
 
@@ -49,28 +59,27 @@ class Combat:
 
     def lancer_combat(self):
         print(f"Le combat entre {self.__pokemon1.get_nom()} et {self.__pokemon2.get_nom()} commence !")
-        while not self.est_finie():
-            print(f"Tour {self.__tour}")
-            if self.attaque_possible():
-                print(f"{self.__pokemon1.get_nom()} attaque {self.__pokemon2.get_nom()}")
+        while not self.fini(): # Tant que le combat n'est pas terminé on continue
+            print(f"Tour {self.__tour}") # On affiche le numéro du tour
+            if self.attaque_possible(): # Si le Pokémon 1 attaque
+                print(f"{self.__pokemon1.get_nom()} attaque {self.__pokemon2.get_nom()}") # On affiche le nom du Pokémon 1 et du Pokémon 2
                 degats = self.calculer_degats(self.__pokemon1, self.__pokemon2)
                 self.enlever_pv(self.__pokemon2, degats)
             else:
                 print(f"{self.__pokemon2.get_nom()} attaque {self.__pokemon1.get_nom()}")
                 degats = self.calculer_degats(self.__pokemon2, self.__pokemon1)
                 self.enlever_pv(self.__pokemon1, degats)
-            self.__tour += 1
+            self.__tour += 1 # On incrémente le numéro du tour
         print(f"Le combat est terminé ! Le vainqueur est {self.get_vainqueur()} !")
         vainqueur = self.get_vainqueur()
-        if vainqueur is not None:
+        if vainqueur is not None: # Si le vainqueur n'est pas None
             pokemon_vainqueur = self.__pokedex.get_pokemon()
             if pokemon_vainqueur is None:
                 # Si le Pokémon n'existe pas dans le Pokedex, on le crée
                 pokemon_vainqueur = eval(vainqueur + "()")
                 self.__pokedex.ajouter_pokemon(pokemon_vainqueur)
             else:
-                # Find the correct Pokemon instance in the list
-                for pokemon in pokemon_vainqueur:
+                for pokemon in pokemon_vainqueur: # Sinon on incrémente le nombre de victoires du Pokémon
                     if pokemon.nom == vainqueur:
                         # Update the number of victories
                         pokemon.set_nb_victoires(pokemon.get_nb_victoires() + 1)
